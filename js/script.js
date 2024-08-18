97b28da8449bbaa3d29fa673e1db94503066d427
chaima garoui <chaimagaroui2@gmail.com>
2024-08-18 18:40:39 +0200

    ajout : css ok

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//selectionner l'élément HTML avec la classe "cp"
+const inputCP = document.querySelector(".cp")
+//szlectionner l'élément HTML avec la classe "ville"
+const inputVille = document.querySelector(".ville")
+
+//on ajoute ensuiste un ecouteur d'événement "input"
